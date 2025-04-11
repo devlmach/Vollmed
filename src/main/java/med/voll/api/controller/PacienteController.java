e97c380c7ba0a0c62aaa35418388b5bd1c07c36b
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-04-11 10:23:51 -0300

    criação paciente + banco de dados

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @PostMapping
+    public void cadastrar(DadosCadastroPaciente dados) {
+        System.out.println(dados);
+    }
+}
