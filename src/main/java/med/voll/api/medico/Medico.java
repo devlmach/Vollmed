efb8540133228927367431a221d3aa3887aed89f
Rafael Machiavelli <rafael.lmachiavelli@cardway.com.br>
2025-04-28 17:21:27 -0300

    list, delete, update medico

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -33,6 +33,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -40,5 +42,25 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dadosAtualizacaoMedico) {
+        if (dadosAtualizacaoMedico.nome() != null) {
+            this.nome = dadosAtualizacaoMedico.nome();
+        }
+
+        if (dadosAtualizacaoMedico.telefone() != null) {
+            this.telefone = dadosAtualizacaoMedico.telefone();
+        }
+
+        if (dadosAtualizacaoMedico.endereco() != null) {
+            this.endereco.atualizarInformacoes(dadosAtualizacaoMedico.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
